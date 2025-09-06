10ee55b4cfa189ccf787ed56c2729481beafa991
Louis Maddox <lmmx@users.noreply.github.com>
2025-09-06 15:12:27 +0100

    schema map inference (#49)
    * feat(schema): add map inference and CLI overrides
    
    * feat: map and record schema (force overriding not working in extension)

--- genson-core/src/lib.rs
+++ genson-core/src/lib.rs
@@ -12,6 +12,29 @@ pub fn infer_json_schema(
     json_strings: &[String],
     config: Option<SchemaInferenceConfig>,
 ) -> Result<SchemaInferenceResult, String> {
+    #[cfg(feature = "trace")]
+    {
+        use crustrace_mermaid::{GroupingMode, MermaidLayer};
+        use tracing_subscriber::filter::LevelFilter;
+        use tracing_subscriber::prelude::*;
+
+        let mmd_layer = MermaidLayer::new()
+            .with_mode(GroupingMode::MergeByName)
+            .with_params_mode(crustrace_mermaid::ParamRenderMode::SingleNodeGrouped);
+
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .with_span_events(
+                        tracing_subscriber::fmt::format::FmtSpan::ENTER
+                            | tracing_subscriber::fmt::format::FmtSpan::EXIT,
+                    )
+                    .with_filter(LevelFilter::INFO),
+            )
+            .with(mmd_layer)
+            .init();
+    }
+
     infer_json_schema_from_strings(json_strings, config.unwrap_or_default())
 }
 
